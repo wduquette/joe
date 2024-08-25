7fcf67e795501b50faac670c54b60d069e74a6c4
Will Duquette <will@wjduquette.com>
2024-08-24 19:28:52 -0700

    Converted `scanToken` to use enhanced switch.

--- lib/src/main/java/com/wjduquette/joe/Scanner.java
+++ lib/src/main/java/com/wjduquette/joe/Scanner.java
@@ -52,62 +52,48 @@ public class Scanner {
     private void scanToken() {
         char c = advance();
         switch (c) {
-            case '(': addToken(LEFT_PAREN); break;
-            case ')': addToken(RIGHT_PAREN); break;
-            case '{': addToken(LEFT_BRACE); break;
-            case '}': addToken(RIGHT_BRACE); break;
-            case ',': addToken(COMMA); break;
-            case '.': addToken(DOT); break;
-            case '-': addToken(MINUS); break;
-            case '+': addToken(PLUS); break;
-            case ';': addToken(SEMICOLON); break;
-            case '*': addToken(STAR); break;
-            case '!':
-                addToken(match('=') ? BANG_EQUAL : BANG);
-                break;
-            case '=':
-                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
-                break;
-            case '<':
-                addToken(match('=') ? LESS_EQUAL : LESS);
-                break;
-            case '>':
-                addToken(match('=') ? GREATER_EQUAL : GREATER);
-                break;
-            case '/':
+            case '(' -> addToken(LEFT_PAREN);
+            case ')' -> addToken(RIGHT_PAREN);
+            case '{' -> addToken(LEFT_BRACE);
+            case '}' -> addToken(RIGHT_BRACE);
+            case ',' -> addToken(COMMA);
+            case '.' -> addToken(DOT);
+            case '-' -> addToken(MINUS);
+            case '+' -> addToken(PLUS);
+            case ';' -> addToken(SEMICOLON);
+            case '*' -> addToken(STAR);
+            case '!' -> addToken(match('=') ? BANG_EQUAL : BANG);
+            case '=' -> addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+            case '<' -> addToken(match('=') ? LESS_EQUAL : LESS);
+            case '>' -> addToken(match('=') ? GREATER_EQUAL : GREATER);
+            case '/' -> {
                 if (match('/')) {
                     // A comment goes until the end of the line.
                     while (peek() != '\n' && !isAtEnd()) advance();
                 } else {
                     addToken(SLASH);
                 }
-                break;
-            case '&':
+            }
+            case '&' -> {
                 if (match('&')) {
                     addToken(AND);
                 } else {
                     Joe.error(line, "Expected '&'.");
                 }
-                break;
-            case '|':
+            }
+            case '|' -> {
                 if (match('|')) {
                     addToken(OR);
                 } else {
                     Joe.error(line, "Expected '|'.");
                 }
-                break;
-            case ' ':
-            case '\r':
-            case '\t':
+            }
+            case ' ', '\r', '\t' -> {
                 // Ignore whitespace.
-                break;
-
-            case '\n':
-                line++;
-                break;
-            case '"': string(); break;
-
-            default:
+            }
+            case '\n' -> line++;
+            case '"' -> string();
+            default -> {
                 if (isDigit(c)) {
                     number();
                 } else if (isAlpha(c)) {
@@ -115,7 +101,7 @@ public class Scanner {
                 } else {
                     Joe.error(line, "Unexpected character: '" + peek() + "'.");
                 }
-                break;
+            }
         }
     }
 
