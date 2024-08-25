149302731efcf1cba0eb2c53ec7e6e27b20b096d
Will Duquette <will@wjduquette.com>
2024-08-24 19:47:26 -0700

    Added `Expr` and preliminary AST printer.

--- lib/src/main/java/com/wjduquette/joe/Expr.java
+++ lib/src/main/java/com/wjduquette/joe/Expr.java
@@ -3,8 +3,8 @@ package com.wjduquette.joe;
 public sealed interface Expr
     permits Expr.Binary, Expr.Grouping, Expr.Literal, Expr.Unary
 {
-    record Binary(Expr left, Token operator, Expr right) implements Expr {}
+    record Binary(Expr left, Token op, Expr right) implements Expr {}
     record Grouping(Expr expr) implements Expr {}
     record Literal(Object value) implements Expr {}
-    record Unary(Token operator, Expr right) implements Expr {}
+    record Unary(Token op, Expr right) implements Expr {}
 }
