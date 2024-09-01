a43f0f7f10fde042764eb7f66d75a959e9837c65
Will Duquette <will@wjduquette.com>
2024-09-01 13:26:39 -0700

    Added initial version of `catch` function.

--- lib/src/main/java/com/wjduquette/joe/StandardLibrary.java
+++ lib/src/main/java/com/wjduquette/joe/StandardLibrary.java
@@ -7,9 +7,35 @@ public class StandardLibrary extends Library {
     public StandardLibrary() {
         super();
 
+        globalFunction("catch",     this::_catch);
         globalFunction("codify",    this::_codify);
         globalFunction("stringify", this::_stringify);
         globalFunction("typeName",  this::_typeName);
+    }
+
+    //**
+    // @global catch
+    // @args callable
+    // @returns error or null
+    // Executes the callable, which must not require any arguments,
+    // and returns `null` if the call succeeds and the error otherwise.
+    //
+    // This is a preliminary implementation of `catch`.  Ultimately
+    // it needs to return a `Pair(ok,error)`.
+    private Object _catch(Joe joe, List<Object> args) {
+        Joe.exactArity(args, 1, "catch(callable)");
+        var arg = args.get(0);
+
+        if (arg instanceof JoeCallable callable) {
+            try {
+                callable.call(joe, List.of());
+                return null;
+            } catch (JoeError ex) {
+                return ex;
+            }
+        } else {
+            throw joe.expected("no-argument callable", arg);
+        }
     }
 
     private Object _codify(Joe joe, List<Object> args) {
