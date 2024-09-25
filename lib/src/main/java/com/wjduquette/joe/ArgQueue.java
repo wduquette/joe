09d7f754adbdd94018c049e30a36b5091e11763a
Will Duquette <will@wjduquette.com>
2024-09-25 14:42:01 -0700

    Fixed bug in `ArgQueue::hasRemaining`

--- lib/src/main/java/com/wjduquette/joe/ArgQueue.java
+++ lib/src/main/java/com/wjduquette/joe/ArgQueue.java
@@ -83,7 +83,7 @@ public final class ArgQueue {
      * @return true or false
      */
     public boolean hasRemaining() {
-        return next >= args.size();
+        return next < args.size();
     }
 
     /**
---
+++ lib/src/test/java/com/wjduquette/joe/ArqQueueTest.java
@@ -0,0 +1,18 @@
+package com.wjduquette.joe;
+
+import org.junit.Test;
+
+import static com.wjduquette.joe.checker.Checker.check;
+
+public class ArqQueueTest extends Ted {
+    ArgQueue args;
+
+    @Test
+    public void testEmpty() {
+        args = new ArgQueue();
+        check(args.size()).eq(0);
+        check(args.isEmpty()).eq(true);
+        check(args.remainingArgs()).eq(0);
+        check(args.hasRemaining()).eq(false);
+    }
+}
