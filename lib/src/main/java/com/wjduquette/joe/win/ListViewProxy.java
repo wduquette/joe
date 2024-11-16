340bf13ab6ce2830a757099df865ebec58b7871a
Will Duquette <will@wjduquette.com>
2024-11-16 10:05:36 -0800

    `joe.win` `ListView` (#37)
    Completes the `joe.win` `ListView` type.
    
    - `JoeListView` takes `Joe` as a constructor argument.
    - The widget can display a list and call the client's onSelect handler.
    - `JoeListView` uses `Joe::stringify` to convert the value to a string.
    - Or, if given, the `JoeListView`'s `stringifier`
    - Which can be set at the script level.
    - Added `ListView` type docs to JUG.

--- lib/src/main/java/com/wjduquette/joe/win/ListViewProxy.java
+++ lib/src/main/java/com/wjduquette/joe/win/ListViewProxy.java
@@ -15,7 +15,7 @@ class ListViewProxy extends FXProxy<JoeListView> {
 
     //**
     // @package joe.win
-    // @type JoeListView
+    // @type ListView
     // @extends Control
     // The `ListView` type is a JavaFX scrolling list widget.
     // Joe classes can extend the `ListView` type.
@@ -36,19 +36,20 @@ class ListViewProxy extends FXProxy<JoeListView> {
         // | Property         | Type            | Description            |
         // | ---------------- | --------------- | ---------------------- |
         // | `#placeholder`   | [[Node]]        | Empty list message     |
-        // | `#selectedIndex` | [[joe.Number]]  | Index of select item   |
-        // | `#selectedItem`  | any             | Value of selected item |
 
-        fxProperty("placeholder", ListView::placeholderProperty, WinPackage::toNode);
+        fxProperty("placeholder", JoeListView::placeholderProperty, WinPackage::toNode);
 
         // Methods
+        method("getSelectedIndex", this::_getSelectedIndex);
+        method("getSelectedItem",  this::_getSelectedItem);
+        method("item",             this::_item);
+        method("items",            this::_items);
         method("onSelect",         this::_onSelect);
         method("placeholder",      this::_placeholder);
         method("placeholderText",  this::_placeholderText);
-        method("item",             this::_item);
-        method("items",            this::_items);
         method("selectIndex",      this::_selectIndex);
         method("selectItem",       this::_selectItem);
+        method("stringifier",      this::_stringifier);
     }
 
     //-------------------------------------------------------------------------
@@ -61,7 +62,7 @@ class ListViewProxy extends FXProxy<JoeListView> {
 
     @Override
     public JoeObject make(Joe joe, JoeClass joeClass) {
-        return new JoeListView(joeClass);
+        return new JoeListView(joe, joeClass);
     }
 
 
@@ -78,6 +79,24 @@ class ListViewProxy extends FXProxy<JoeListView> {
 
     //-------------------------------------------------------------------------
     // Methods
+
+    //**
+    // @method getSelectedIndex
+    // @result joe.Number
+    // Gets the index of the selected item, or -1 if there is no selection.
+    private Object _getSelectedIndex(JoeListView node, Joe joe, Args args) {
+        args.exactArity(0, "getSelectedIndex()");
+        return (double)node.getSelectionModel().getSelectedIndex();
+    }
+
+    //**
+    // @method getSelectedItem
+    // @result item
+    // Gets the selected item, or `null` if there is no selection.
+    private Object _getSelectedItem(JoeListView node, Joe joe, Args args) {
+        args.exactArity(0, "getSelectedItem()");
+        return node.getSelectionModel().getSelectedItem();
+    }
 
     //**
     // @method item
@@ -99,6 +118,13 @@ class ListViewProxy extends FXProxy<JoeListView> {
         return joe.wrapList(node.getItems(), Object.class);
     }
 
+    //**
+    // @method onSelect
+    // @args callable
+    // @result this
+    // Specifies a callable to be called when the user selects an
+    // item in the list.  The callable must take one argument,
+    // the `ListView` itself.
     private Object _onSelect(JoeListView node, Joe joe, Args args) {
         args.exactArity(1, "onSelect(callable)");
         var handler = args.next();
@@ -111,17 +137,37 @@ class ListViewProxy extends FXProxy<JoeListView> {
         return node;
     }
 
+    //**
+    // @method placeholder
+    // @args node
+    // @result this
+    // Sets the widget's placeholder graphic, a [[Node]] to display when
+    // the widget's [[ListView#method.items]] list is empty.
     private Object _placeholder(JoeListView node, Joe joe, Args args) {
         args.exactArity(1, "placeholder(node)");
         node.setPlaceholder(WinPackage.toNode(joe, args.next()));
         return node;
     }
+
+    //**
+    // @method placeholderText
+    // @args text
+    // @result this
+    // Sets the widget's placeholder graphic to a label displaying
+    // the given text. The placeholder is shown when
+    // the widget's [[ListView#method.items]] list is empty.
     private Object _placeholderText(JoeListView node, Joe joe, Args args) {
         args.exactArity(1, "placeholderText(text)");
         node.setPlaceholder(new Label(joe.stringify(args.next())));
         return node;
     }
 
+    //**
+    // @method selectIndex
+    // @args index
+    // @result this
+    // Selects the item at the given *index*.  Throws an error if the
+    // index is not in range.
     private Object _selectIndex(JoeListView node, Joe joe, Args args) {
         args.exactArity(1, "selectIndex(index)");
         var index = joe.toIndex(args.next(), node.getItems().size());
@@ -129,10 +175,35 @@ class ListViewProxy extends FXProxy<JoeListView> {
         return node;
     }
 
+    //**
+    // @method selectItem
+    // @args item
+    // @result this
+    // Selects the given *item*.  The call is a no-op if the *item*
+    // isn't contained in the widget's list of items.
     private Object _selectItem(JoeListView node, Joe joe, Args args) {
         args.exactArity(1, "selectItem(item)");
         node.selectItem(args.next());
         return node;
     }
 
+    //**
+    // @method stringifier
+    // @args callable
+    // @result this
+    // Sets the widget's stringifier to the given *callable*, which
+    // must take one argument, a list item, and return a string
+    // representation for that item.
+    private Object _stringifier(JoeListView node, Joe joe, Args args) {
+        args.exactArity(1, "stringifier(callable)");
+
+        var handler = args.next();
+
+        if (handler instanceof JoeCallable callable) {
+            node.setStringifier(v -> joe.call(callable, v).toString());
+        } else {
+            throw joe.expected("callable", handler);
+        }
+        return node;
+    }
 }
