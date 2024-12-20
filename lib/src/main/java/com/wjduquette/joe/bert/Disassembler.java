1a362158d47bca8776d4bdc28162831c07c58b48
Will Duquette <will@wjduquette.com>
2024-12-20 08:38:05 -0800

    Revised the disassembler to handler multiple line instructions.

--- lib/src/main/java/com/wjduquette/joe/bert/Disassembler.java
+++ lib/src/main/java/com/wjduquette/joe/bert/Disassembler.java
@@ -3,6 +3,8 @@ package com.wjduquette.joe.bert;
 import com.wjduquette.joe.Joe;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.wjduquette.joe.bert.Opcode.*;
 
@@ -56,41 +58,35 @@ public class Disassembler {
             buff.append("\n");
         }
 
-        // NEXT, get the two data columns
-        var left = new ArrayList<String>();
-        var right = new ArrayList<String>();
-
-        var source = chunk.source();
-        left.add(String.format("%04d", chunk.span().startLine()));
-        right.add(source.line(chunk.span().startLine()));
-
+        // NEXT, get the lines of the output.
+        var lines = new ArrayList<Line>();
         for (int ip = 0; ip < chunk.codeSize(); ) {
-            if (chunk.source() != null && startsNewLine(ip)) {
-                right.add(source.line(chunk.line(ip)));
-            } else {
-                right.add("");
-            }
-            var pair = instruction(ip);
-            left.add(chunkPrefix(ip) + pair.result);
-
-            ip = pair.next;
+            ip = decode(lines, ip, true);
         }
 
-        var leftWidth = left.stream()
+        // NEXT, get the width of the left column.
+        var leftMax = lines.stream()
+            .map(Line::text)
             .mapToInt(String::length)
             .max()
             .orElse(0);
+        var leftWidth = Math.max(leftMax, 40);
 
-        for (var i = 0; i < left.size(); i++) {
-            if (right.get(i).isEmpty()) {
-                buff.append(left.get(i))
-                    .append("\n");
-            } else {
-                buff.append(pad(left.get(i), leftWidth))
-                    .append(" ; ")
-                    .append(right.get(i))
-                    .append("\n");
+        // NEXT, output each line with its source.
+        var source = chunk.source();
+        buff.append(pad(String.format("%04d", chunk.span().startLine()), leftWidth))
+            .append("  ")
+            .append(source.line(chunk.span().startLine()))
+            .append("\n");
+
+        for (var line : lines) {
+            buff.append(pad(line.text, leftWidth));
+
+            if (startsNewLine(line.ip)) {
+                buff.append("  ")
+                    .append(source.line(chunk.line(line.ip)));
             }
+            buff.append("\n");
         }
 
         return buff.toString();
@@ -104,77 +100,88 @@ public class Disassembler {
 
     String disassembleInstruction(CodeChunk chunk, int ip) {
         this.chunk = chunk;
-        return singlePrefix(ip) + instruction(ip).result;
+        var lines = new ArrayList<Line>();
+        decode(lines, ip, false);
+        return lines.stream()
+            .map(Line::text)
+            .collect(Collectors.joining("\n"));
     }
 
-    private Pair instruction(int ip) {
+
+    // Decodes one or more lines starting at ip. Returns the next
+    // ip to decode.
+    private int decode(List<Line> lines, int ip, boolean isChunk) {
         var opcode = chunk.code(ip);
-        return switch (opcode) {
-            case CALL, LOCGET, LOCSET, UPGET, UPSET
-                -> charInstruction(ip);
-            case JIF, JIFKEEP, JITKEEP, JUMP
-                -> jumpInstruction(ip, 1);
-            case LOOP
-                -> jumpInstruction(ip, -1);
-            case CONST, GLODEF, GLOGET, GLOSET
-                -> constantInstruction(ip);
+        var prefix = isChunk ? chunkPrefix(ip) : singlePrefix(ip);
+
+        switch (opcode) {
+            case CALL, LOCGET, LOCSET, UPGET, UPSET -> {
+                // Pattern: opcode charValue
+                char arg = chunk.code(ip + 1);
+                var text = String.format(" %04d", (int)arg);
+                lines.add(new Line(ip, prefix + text));
+                return ip + 2;
+            }
+            case JIF, JIFKEEP, JITKEEP, JUMP -> {
+                // Pattern: opcode jumpOffset (forwards)
+                char jump = chunk.code(ip + 1);
+                var text = String.format(" %04d -> %d", (int)jump,
+                    ip + 2 + jump);  // Add jump
+                lines.add(new Line(ip, prefix + text));
+                return ip + 2;
+            }
+            case LOOP -> {
+                // Pattern: opcode jumpOffset (backwards)
+                char jump = chunk.code(ip + 1);
+                var text = String.format(" %04d -> %d", (int)jump,
+                    ip + 2 - jump); // Subtract jump
+                lines.add(new Line(ip, prefix + text));
+                return ip + 2;
+            }
+            case CONST, GLODEF, GLOGET, GLOSET -> {
+                // Pattern: opcode constantIndex
+                int index = chunk.code(ip + 1);
+                var constant = joe.stringify(chunk.getConstant(index));
+                if (constant.length() > 10) {
+                    constant = constant.substring(0, 7) + "...";
+                }
+                var text = String.format(" %04d '%s'", index, constant);
+                lines.add(new Line(ip, prefix + text));
+                return ip + 2;
+            }
             case ADD, DIV, EQ, FALSE, GE, GT, LE, LT, MUL,
                 NE, NEGATE, NOT, NULL, POP, PRINT, RETURN, SUB, TRUE
-                -> simpleInstruction(ip);
-            case CLOSURE -> closureInstruction(ip);
-            default -> unknownOpcode(ip);
-        };
-    }
+            -> {
+                // Pattern: opcode
+                lines.add(new Line(ip, prefix));
+                return ip + 1;
+            }
+            case CLOSURE -> {
+                var start = ip++;
+                int constIndex = chunk.code(ip++);
+                var func = (Function)chunk.getConstant(constIndex);
+                var text = String.format(" %04d <fn %s>", constIndex, func.name());
 
-    private Pair charInstruction(int ip) {
-        char arg = chunk.code(ip + 1);
-        var text = String.format(" %04d", (int)arg);
-        return new Pair(text, ip + 2);
-    }
+                lines.add(new Line(start, prefix + text));
 
-    private Pair closureInstruction(int ip) {
-        ip++;
-        int constIndex = chunk.code(ip++);
-        var func = (Function)chunk.getConstant(constIndex);
-        var buff = new StringBuilder();
-        buff.append(String.format(" %04d '%s'", constIndex, func.toString()));
+                for (var i = 0; i < func.upvalueCount; i++) {
+                    int isLocal = chunk.code(ip);
+                    int index = chunk.code(ip);
+                    var upText = String.format("   |  @%04d         %s %d",
+                        ip, isLocal == 1 ? "local" : "upvalue", index);
+                    lines.add(new Line(ip, upText));
+                    ip += 2;
+                }
 
-        for (var i = 0; i < func.upvalueCount; i++) {
-            int isLocal = chunk.code(ip++);
-            int index = chunk.code(ip++);
-            buff.append(String.format("\n   |  @%04d         %s %d",
-                ip - 2, isLocal == 1 ? "local" : "upvalue", index));
+                return ip;
+            }
+            default -> {
+                // Pattern: opcode (unknown, presume no argument)
+                var text = String.format(" %03d", (int)opcode);
+                lines.add(new Line(ip, prefix + text));
+                return ip + 1;
+            }
         }
-        return new Pair(buff.toString(), ip);
-    }
-
-
-    private Pair constantInstruction(int ip) {
-        int index = chunk.code(ip + 1);
-        var constant = joe.stringify(chunk.getConstant(index));
-        if (constant.length() > 10) {
-            constant = constant.substring(0, 7) + "...";
-        }
-        var text = String.format(" %04d '%s'", index, constant);
-        return new Pair(text, ip + 2);
-    }
-
-    private Pair jumpInstruction(int ip, int sign) {
-        char jump = chunk.code(ip + 1);
-        var text = String.format(" %04d -> %d", (int)jump,
-            ip + 2 + sign*jump);
-        return new Pair(text, ip + 2);
-    }
-
-
-    private Pair simpleInstruction(int ip) {
-        return new Pair("", ip + 1);
-    }
-
-    private Pair unknownOpcode(int ip) {
-        var opcode = chunk.code(ip);
-        var text = String.format(" %03d", (int)opcode);
-        return new Pair(text, ip + 1);
     }
 
     private String singlePrefix(int ip) {
@@ -203,6 +210,6 @@ public class Disassembler {
     //-------------------------------------------------------------------------
     // Helpers
 
-    // The result of disassembling a single instruction.
-    private record Pair(String result, int next) { }
+    // A line of disassembly output, tied to its index in the code.
+    private record Line(int ip, String text) {}
 }
