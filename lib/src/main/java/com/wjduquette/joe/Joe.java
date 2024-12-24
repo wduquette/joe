07c154a98d3fc7a0ba0e3b275f0ec62debdd06a0
Will Duquette <will@wjduquette.com>
2024-12-24 08:03:57 -0800

    Cleaned up temporary debugging code.

--- lib/src/main/java/com/wjduquette/joe/Joe.java
+++ lib/src/main/java/com/wjduquette/joe/Joe.java
@@ -1,7 +1,6 @@
 package com.wjduquette.joe;
 
 import com.wjduquette.joe.bert.BertEngine;
-import com.wjduquette.joe.tools.test.TestPackage;
 import com.wjduquette.joe.types.*;
 import com.wjduquette.joe.walker.WalkerEngine;
 
@@ -79,14 +78,7 @@ public class Joe {
     public Joe(String engineType) {
         switch (engineType) {
             case WALKER -> engine = new WalkerEngine(this);
-            case BERT -> {
-                engine = new BertEngine(this);
-                // TEMPORARY
-                installGlobalFunction("joeCall", (j,a) -> {
-                    a.exactArity(1, "joeCall(callable)");
-                    return call(a.next());
-                });
-            }
+            case BERT -> engine = new BertEngine(this);
             default -> throw new IllegalArgumentException(
                 "Invalid Engine type: '" + engineType + "'.");
         }
