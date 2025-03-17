d020369b149cf1def5252925205a4f25898c6b64
Will Duquette <will@wjduquette.com>
2025-03-17 10:01:48 -0700

    Added preliminary `WalkerRecord`, `WalkerRecordValue` classes.

---
+++ lib/src/main/java/com/wjduquette/joe/walker/WalkerRecordValue.java
@@ -0,0 +1,92 @@
+package com.wjduquette.joe.walker;
+
+import com.wjduquette.joe.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class WalkerRecordValue implements JoeValue {
+    private final static String TO_STRING = "toString";
+
+    //-------------------------------------------------------------------------
+    // Instance Variables
+
+    private final WalkerRecord type;
+    private final Map<String, Object> fields;
+
+    // Default "toString()" implementation.
+    private final NativeCallable _toString;
+
+    WalkerRecordValue(WalkerRecord type, Map<String,Object> fields) {
+        this.type = type;
+        this.fields = fields;
+        this._toString = new NativeMethod<>(this, "toString",
+            (objc, joe, args) -> toStringRep(joe));
+    }
+
+    private String toStringRep(Joe joe) {
+        var values = type.getRecordFields().stream()
+            .map(n -> joe.stringify(fields.get(n)))
+            .collect(Collectors.joining(", "));
+        return type.name() + "(" + values + ")";
+    }
+
+    //-------------------------------------------------------------------------
+    // JoeValue API
+
+    @Override
+    public JoeType type() {
+        return type;
+    }
+
+    @Override
+    public String typeName() {
+        return type.name();
+    }
+
+    @Override
+    public boolean hasField(String name) {
+        return fields.containsKey(name);
+    }
+
+    @Override
+    public List<String> getFieldNames() {
+        return type.getRecordFields();
+    }
+
+    @Override
+    public Object get(String name) {
+        if (fields.containsKey(name)) {
+            return fields.get(name);
+        }
+
+        JoeCallable method = type.bind(this, name);
+
+        if (method != null) return method;
+
+        if (name.equals(TO_STRING)) {
+            return _toString;
+        }
+
+        throw new JoeError("Undefined property '" + name + "'.");
+    }
+
+    @Override
+    public void set(String name, Object value) {
+        throw new JoeError("Values of type " + type.name() +
+            " have no mutable properties.");
+    }
+
+    @Override
+    public String stringify(Joe joe) {
+        var callable = get(TO_STRING);
+        return (String)joe.call(callable);
+    }
+
+    @Override
+    public String toString() {
+        // This is for debugging only; it isn't used for the string rep.
+        return "<" + type.name() + "@" + String.format("%x",hashCode()) + ">";
+    }
+}
