c917cb2e3dcd44b11a049fd378f88ef7f5fe3a06
Will Duquette <will@wjduquette.com>
2025-03-22 09:17:23 -0700

    `if let` and `match` bugs (#64)
    Both the `if let` and `match` statements had variable resolution errors in the Walker engine:
    
    - The `Resolver` defined a new scope *before* resolving the match target and the pattern constants (as it should).
    - The `Interpreter` created the matching `Environment` *after* evaluating the match target and the pattern constants.
    
    Fixed, and added relevant tests to `lang_if_let.joe` and `lang_match.joe`.

--- lib/src/main/java/com/wjduquette/joe/walker/Interpreter.java
+++ lib/src/main/java/com/wjduquette/joe/walker/Interpreter.java
@@ -193,27 +193,29 @@ class Interpreter {
                 }
             }
             case Stmt.IfLet stmt -> {
-                var constants = new ArrayList<>();
-                stmt.pattern().getConstants().forEach(e ->
-                    constants.add(evaluate(e)));
-                var target = evaluate(stmt.target());
-                var values = new ArrayList<>();
-                if (Matcher.bind(
-                    joe,
-                    stmt.pattern().getPattern(),
-                    target,
-                    constants::get,
-                    values::add
-                )) {
-                    var previous = this.environment;
-                    try {
-                        this.environment = new Environment(previous);
+                var previous = this.environment;
+                this.environment = new Environment(previous);
+                try {
+                    var constants = new ArrayList<>();
+                    stmt.pattern().getConstants().forEach(e ->
+                        constants.add(evaluate(e)));
+                    var target = evaluate(stmt.target());
+                    var values = new ArrayList<>();
+                    if (Matcher.bind(
+                        joe,
+                        stmt.pattern().getPattern(),
+                        target,
+                        constants::get,
+                        values::add
+                    )) {
                         bind(stmt.pattern(), values);
                         return execute(stmt.thenBranch());
-                    } finally {
-                        this.environment = previous;
                     }
-                } else if (stmt.elseBranch() != null) {
+                } finally {
+                    this.environment = previous;
+                }
+
+                if (stmt.elseBranch() != null) {
                     return execute(stmt.elseBranch());
                 }
             }
@@ -243,25 +245,25 @@ class Interpreter {
                     if (c.pattern() == null) return execute(c.statement());
 
                     // case pattern ->
-                    var constants = new ArrayList<>();
-                    c.pattern().getConstants().forEach(e ->
-                        constants.add(evaluate(e)));
-                    var values = new ArrayList<>();
-                    if (Matcher.bind(
-                        joe,
-                        c.pattern().getPattern(),
-                        target,
-                        constants::get,
-                        values::add
-                    )) {
-                        var previous = this.environment;
-                        try {
-                            this.environment = new Environment(previous);
+                    var previous = this.environment;
+                    try {
+                        this.environment = new Environment(previous);
+                        var constants = new ArrayList<>();
+                        c.pattern().getConstants().forEach(e ->
+                            constants.add(evaluate(e)));
+                        var values = new ArrayList<>();
+                        if (Matcher.bind(
+                            joe,
+                            c.pattern().getPattern(),
+                            target,
+                            constants::get,
+                            values::add
+                        )) {
                             bind(c.pattern(), values);
                             return execute(c.statement());
-                        } finally {
-                            this.environment = previous;
                         }
+                    } finally {
+                        this.environment = previous;
                     }
                 }
             }
