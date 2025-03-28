e170b68c0566e545a1f00d85612e83b961c75521
Will Duquette <will@wjduquette.com>
2025-03-28 11:48:06 -0700

    Added `TupleIterator`.

--- lib/src/main/java/com/wjduquette/joe/nero/RuleSet.java
+++ lib/src/main/java/com/wjduquette/joe/nero/RuleSet.java
@@ -42,8 +42,7 @@ public class RuleSet {
      */
     public boolean addFact(Fact fact) {
         if (!knownFacts.contains(fact)) {
-            var list = factMap.computeIfAbsent(fact.relation(),
-                key -> new ArrayList<>());
+            var list = factsFor(fact.relation());
             list.add(fact);
 
             knownFacts.add(fact);
@@ -53,15 +52,87 @@ public class RuleSet {
         }
     }
 
+    public List<Fact> factsFor(String relation) {
+        return factMap.computeIfAbsent(relation,
+            key -> new ArrayList<>());
+    }
+
     /**
      * Executes the inference algorithm, computing all facts knowable
      * from the rules.
      */
     public void ponder() {
-
+        for (var rule : rules) {
+            System.out.println("Rule: " + rule);
+            var iter = new TupleIterator(rule);
+            while (iter.hasNext()) {
+                var tuple = iter.next();
+                System.out.println("  " + Arrays.toString(tuple));
+            }
+        }
     }
 
     public Set<Fact> getKnownFacts() {
         return Collections.unmodifiableSet(knownFacts);
+    }
+
+    //-------------------------------------------------------------------------
+    // TupleIterator
+
+    private class TupleIterator {
+        //---------------------------------------------------------------------
+        // Instance Variables
+
+        private final List<List<Fact>> inputs = new ArrayList<>();
+        private Fact[] tuple = null;
+        private final int total;
+        private int next = 0;
+
+        //---------------------------------------------------------------------
+        // Constructor
+
+        TupleIterator(Rule rule) {
+            // FIRST, get the lists of facts in the order referenced in the
+            // rule's body.
+            int sum = 0;
+            for (var item : rule.body()) {
+                var facts = factsFor(item.relation());
+
+                if (facts.isEmpty()) {
+                    inputs.clear();
+                    total = 0;
+                    return;
+                }
+                inputs.add(facts);
+                sum += facts.size();
+            }
+
+            // NEXT, save the max number of tuples
+            total = sum;
+            tuple = new Fact[inputs.size()];
+        }
+
+        //---------------------------------------------------------------------
+        // API
+
+        public boolean hasNext() {
+            return total != next;
+        }
+
+        public Fact[] next() {
+            if (!hasNext()) {
+                throw new IllegalStateException("Iterator is empty.");
+            }
+
+            var n = next++;
+
+            for (var i = 0; i < inputs.size(); i++) {
+                var input = inputs.get(i);
+                tuple[i] = input.get(n % input.size());
+                n /= input.size();
+            }
+
+            return tuple;
+        }
     }
 }
