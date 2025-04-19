b6f7af1e0fcff8f99c5f6c67754acabe33108939
Will Duquette <will@wjduquette.com>
2025-04-19 08:36:29 -0700

    Fixed function compilation error; parameters are now properly defined.

--- lib/src/main/java/com/wjduquette/joe/clark/Compiler.java
+++ lib/src/main/java/com/wjduquette/joe/clark/Compiler.java
@@ -352,6 +352,7 @@ class Compiler {
         for (var param : params) {
             var constant = addVariable(param);
             defineVariable(constant);
+            current.parameters.add(param.lexeme());
         }
 
         compile(body);
