3e83147472599f18d6091002213fa361a14e0b67
Will Duquette <will@wjduquette.com>
2025-05-09 08:59:41 -0700

    Bert now has its own scanner/tokens, so that it is unaffected by Walker/Clark evolution.

---
+++ lib/src/main/java/com/wjduquette/joe/bert/Token.java
@@ -0,0 +1,46 @@
+package com.wjduquette.joe.bert;
+
+import com.wjduquette.joe.SourceBuffer.Span;
+
+/**
+ * A token produce by Bert's scanner.
+ * @param type The token type
+ * @param span The span in the source code
+ * @param literal A literal value, e.g., for numbers, or null
+ */
+public record Token(
+    TokenType type,
+    Span span,
+    Object literal
+) {
+    /**
+     * Creates a synthetic token representing a specific lexeme.
+     * @param lexeme The lexeme.
+     * @return The token.
+     */
+    public static Token synthetic(String lexeme) {
+        return new Token(null, null, lexeme);
+    }
+
+    /**
+     * Returns the token's lexeme.
+     * @return The lexeme
+     */
+    public String lexeme() {
+        return span != null ? span.text() : (String)literal;
+    }
+
+    /**
+     * Returns the starting line number for the token's span, or -1
+     * for synthetic tokens.
+     * @return The line number.
+     */
+    public int line() {
+        return span != null ? span.startLine() : -1;
+    }
+
+    @Override
+    public String toString() {
+        return type + "[" + lexeme() + "," + literal + "," + line() + "]";
+    }
+}
