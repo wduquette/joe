777aa4a62132b799b71c46b652ef77c7d586f6cf
Will Duquette <will@wjduquette.com>
2025-07-05 09:23:03 -0700

    Revised `RuleEngine` to use faster algorithm.

--- lib/src/main/java/com/wjduquette/joe/nero/RuleEngine.java
+++ lib/src/main/java/com/wjduquette/joe/nero/RuleEngine.java
@@ -220,13 +220,129 @@ public class RuleEngine {
             if (debug) System.out.println("Iteration " + stratum + "." + (++count) + ":");
             for (var head : heads) {
                 for (var rule : ruleMap.get(head)) {
-                    if (matchRule(rule)) {
-                        gotNewFact = true;
-                    }
+                    if (matchRule2(rule)) gotNewFact = true;
                 }
             }
         } while (gotNewFact);
     }
+
+    // Matches the rule against the known facts, adding any new facts and
+    // returning true if there were any and false otherwise.
+    private boolean matchRule2(Rule rule) {
+        if (debug) System.out.println("  Rule: " + rule);
+
+        var bc = new BindingContext(rule);
+
+        matchBodyAtom(bc, 0);
+
+        if (!bc.inferredFacts.isEmpty()) {
+            knownFacts.addAll(bc.inferredFacts);
+            inferredFacts.addAll(bc.inferredFacts);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static class BindingContext {
+        private final Rule rule;
+        private final List<Fact> inferredFacts = new ArrayList<>();
+        private Bindings bindings = new Bindings();
+
+        BindingContext(Rule rule) {
+            this.rule = rule;
+        }
+    }
+
+    // Matches the rule's index-th body atom against the relevant facts.
+    private void matchBodyAtom(BindingContext bc, int index) {
+        var atom = bc.rule.body().get(index);
+        var facts = knownFacts.getRelation(atom.relation());
+
+        // FIRST, Save the current bindings, as we will begin with them for each
+        // fact.
+        var givenBindings = bc.bindings;
+
+        for (var fact : facts) {
+            // FIRST, Copy the bindings for this fact.
+            bc.bindings = new Bindings(givenBindings);
+
+            // NEXT, Verify that the fact can be matched by this atom.
+            if (atom.requiresOrderedFields() && !fact.isOrdered()) {
+                throw new JoeError(
+                    "'" + atom.relation() +
+                        "' in rule '" + bc.rule +
+                        "' requires ordered fields, but a provided " +
+                        "fact is not ordered.");
+            }
+
+            // NEXT, if it doesn't match, go on to the next fact.
+            // TODO: Improve the `BodyAtom::matches` API
+            bc.bindings = atom.matches(fact, bc.bindings);
+            if (bc.bindings == null) continue;
+
+            // NEXT, it matches.  If there's another body atom, check it and
+            // then go on to the next fact.
+            if (index + 1 < bc.rule.body().size()) {
+                matchBodyAtom(bc, index + 1);
+                continue;
+            }
+
+            // NEXT, we've matched all body atoms.  Check the bindings against
+            // the constraints.  If they are not met, continue with the
+            // next fact.
+            if (!constraintsMet(bc)) continue;
+
+            // NEXT, check each negation.
+            if (!checkNegations(bc)) continue;
+
+            // NEXT, the rule has matched.  Build the inferred fact, and see
+            // if it's actually new.
+            var newFact = createFact(bc);
+            if (!knownFacts.getAll().contains(newFact)) {
+                bc.inferredFacts.add(newFact);
+                if (debug) System.out.println("      Fact: " + newFact);
+            }
+        }
+    }
+
+    private boolean constraintsMet(BindingContext bc) {
+        for (var constraint : bc.rule.constraints()) {
+            if (!constraintMet(constraint, bc.bindings)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkNegations(BindingContext bc) {
+        for (var atom : bc. rule.negations()) {
+            for (var fact : knownFacts.getRelation(atom.relation())) {
+                if (atom.matches(fact, bc.bindings) != null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private Fact createFact(BindingContext bc) {
+        var terms = new ArrayList<>();
+
+        for (var term : bc.rule.head().terms()) {
+            switch (term) {
+                case Constant c -> terms.add(c.value());
+                case Variable v -> terms.add(bc.bindings.get(v));
+                case Wildcard ignored -> throw new IllegalStateException(
+                    "Rule head contains a Wildcard term.");
+            }
+        }
+
+        return factFactory.create(bc.rule.head().relation(), terms);
+    }
+
+    //-------------------------------------------------------------------------
+    // Old Rule Matching
 
     private boolean matchRule(Rule rule) {
         if (debug) System.out.println("  Rule: " + rule);
