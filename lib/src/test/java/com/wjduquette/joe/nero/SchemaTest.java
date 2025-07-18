126d5b534142198503b3a55715f6d31568eb5fdb
Will Duquette <will@wjduquette.com>
2025-07-18 09:20:56 -0700

    `SchemaTest` now tests `checkAndAdd(ASTAtom head)`, now that `ASTRuleSet` includes the `Schema`.

--- lib/src/test/java/com/wjduquette/joe/nero/SchemaTest.java
+++ lib/src/test/java/com/wjduquette/joe/nero/SchemaTest.java
@@ -127,7 +127,120 @@ public class SchemaTest extends Ted {
     //-------------------------------------------------------------------------
     // checkAndAdd(ASTATom)
 
-    // Add checkAndAdd(ASTAtom) tests once `RuleSet` includes a schema.
+    // A head atom's shape is always accepted if the relation isn't previously
+    // known.
+    @Test public void testCheckAndAdd_head_new() {
+        test("testCheckAndAdd_head_new");
+
+        var ast = parse("Person(#a, #b);");
+        var head = ast.axioms().getFirst();
+        var shape = Shape.infer(head);
+        check(schema.checkAndAdd(head)).eq(true);
+
+        check(schema.getRelations()).eq(Set.of("Person"));
+        check(schema.hasRelation("Person")).eq(true);
+        check(schema.get("Person")).eq(shape);
+    }
+
+    // For a known list shape, a head atom must be ordered with the correct
+    // arity.
+    @Test public void testCheckAndAdd_head_list_ok() {
+        test("testCheckAndAdd_head_list_ok");
+
+        var shape = new Shape.ListShape("Person", 2);
+        schema.checkAndAdd(shape);
+
+        var ast = parse("Person(#a, #b);");
+        var head = ast.axioms().getFirst();
+        check(schema.checkAndAdd(head)).eq(true);
+
+        check(schema.getRelations()).eq(Set.of("Person"));
+        check(schema.hasRelation("Person")).eq(true);
+        check(schema.get("Person")).eq(shape);
+    }
+
+    // For a known list shape, a head atom must be ordered with the correct
+    // arity.
+    @Test public void testCheckAndAdd_head_list_mismatch() {
+        test("testCheckAndAdd_head_list_mismatch");
+
+        var shape = new Shape.ListShape("Person", 2);
+        schema.checkAndAdd(shape);
+
+        var ast = parse("Person(#a, #b, #c);");
+        var head = ast.axioms().getFirst();
+        check(schema.checkAndAdd(head)).eq(false);
+
+        check(schema.getRelations()).eq(Set.of("Person"));
+        check(schema.hasRelation("Person")).eq(true);
+        check(schema.get("Person")).eq(shape);
+    }
+
+    // For a known map shape, a head atom must be named.
+    @Test public void testCheckAndAdd_head_map_ok() {
+        test("testCheckAndAdd_head_map_ok");
+
+        var shape = new Shape.MapShape("Thing");
+        schema.checkAndAdd(shape);
+
+        var ast = parse("Thing(a: #a, b: #b);");
+        var head = ast.axioms().getFirst();
+        check(schema.checkAndAdd(head)).eq(true);
+
+        check(schema.getRelations()).eq(Set.of("Thing"));
+        check(schema.hasRelation("Thing")).eq(true);
+        check(schema.get("Thing")).eq(shape);
+    }
+
+    // For a known map shape, a head atom must be named.
+    @Test public void testCheckAndAdd_head_map_mismatch() {
+        test("testCheckAndAdd_head_map_mismatch");
+
+        var shape = new Shape.MapShape("Thing");
+        schema.checkAndAdd(shape);
+
+        var ast = parse("Thing(#a, #b);");
+        var head = ast.axioms().getFirst();
+        check(schema.checkAndAdd(head)).eq(false);
+
+        check(schema.getRelations()).eq(Set.of("Thing"));
+        check(schema.hasRelation("Thing")).eq(true);
+        check(schema.get("Thing")).eq(shape);
+    }
+
+    // For a known pair shape, a head atom must be ordered with the correct
+    // arity.
+    @Test public void testCheckAndAdd_head_pair_ok() {
+        test("testCheckAndAdd_head_pair_ok");
+
+        var shape = new Shape.PairShape("Person", List.of("a", "b"));
+        schema.checkAndAdd(shape);
+
+        var ast = parse("Person(#a, #b);");
+        var head = ast.axioms().getFirst();
+        check(schema.checkAndAdd(head)).eq(true);
+
+        check(schema.getRelations()).eq(Set.of("Person"));
+        check(schema.hasRelation("Person")).eq(true);
+        check(schema.get("Person")).eq(shape);
+    }
+
+    // For a known list shape, a head atom must be ordered with the correct
+    // arity.
+    @Test public void testCheckAndAdd_head_pair_mismatch() {
+        test("testCheckAndAdd_head_pair_mismatch");
+
+        var shape = new Shape.PairShape("Person", List.of("a", "b"));
+        schema.checkAndAdd(shape);
+
+        var ast = parse("Person(#a, #b, #c);");
+        var head = ast.axioms().getFirst();
+        check(schema.checkAndAdd(head)).eq(false);
+
+        check(schema.getRelations()).eq(Set.of("Person"));
+        check(schema.hasRelation("Person")).eq(true);
+        check(schema.get("Person")).eq(shape);
+    }
 
     //-------------------------------------------------------------------------
     // inferSchema
