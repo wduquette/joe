05dc7fa963a8c172848d9caf2768b48d6f929d68
Will Duquette <will@wjduquette.com>
2025-08-02 10:20:33 -0700

    Bindings now maps `Variable` *names* to values, not `Variables` to values.

--- lib/src/main/java/com/wjduquette/joe/nero/Bindings.java
+++ lib/src/main/java/com/wjduquette/joe/nero/Bindings.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * A map of variable bindings, used while matching
  * {@link Atom BodyAtoms} against {@link Fact Facts}.
  */
-public class Bindings extends HashMap<Variable,Object> {
+public class Bindings extends HashMap<String,Object> {
     /**
      * Creates an empty set of bindings.
      */
@@ -19,7 +19,7 @@ public class Bindings extends HashMap<Variable,Object> {
      * Creates a set of bindings, initializing with the other bindings.
      * @param other The other bindings.
      */
-    public Bindings(Map<Variable,Object> other) {
+    public Bindings(Map<String,Object> other) {
         super(other);
     }
 }
