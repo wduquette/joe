e1f5027717af65fecf9a7094c26d2e353c4432e5
Will Duquette <will@wjduquette.com>
2025-11-01 07:22:05 -0700

    Added `jug/javadoc.sh`, to build the Joe javadoc.

---
+++ jug/javadoc.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+rm -rf ../docs/javadoc
+mkdir -p ../docs/javadoc
+javadoc -sourcepath ../lib/src/main/java   \
+    -subpackages com.wjduquette.joe        \
+    -d ../docs/javadoc                     \
+    -doctitle "Joe Language Java API"      \
+    -windowtitle "Joe Language Java API"   \
+    -javafx                                \
+    -link https://docs.oracle.com/en/java/javase/21/docs/api/ \
+    -link https://openjfx.io/javadoc/21/ \
+    -quiet
+
+
