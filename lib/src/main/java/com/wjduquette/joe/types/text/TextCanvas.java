43068f312b08b0c3a2d294d8f816f8708a9c68f5
Will Duquette <will@wjduquette.com>
2025-12-30 08:08:14 -0800

    Added unicode characters.

--- lib/src/main/java/com/wjduquette/joe/types/text/TextCanvas.java
+++ lib/src/main/java/com/wjduquette/joe/types/text/TextCanvas.java
@@ -16,14 +16,19 @@ public class TextCanvas {
     public static final char BLANK = ' ';
 
     // From Unicode Box Drawing, Block Elements, Geometric Figures, 2500-25FF
-    /** Unicode character */ public static final String LIGHT_HORIZONTAL = "\u2500";
-    /** Unicode character */ public static final String LIGHT_VERTICAL = "\u2502";
-    /** Unicode character */ public static final String LIGHT_DOWN_AND_HORIZONTAL = "\u252C";
-    /** Unicode character */ public static final String LIGHT_UP_AND_HORIZONTAL = "\u2534";
-    /** Unicode character */ public static final String LIGHT_VERTICAL_AND_LEFT = "\u2524";
-    /** Unicode character */ public static final String WHITE_UP_POINTING_TRIANGLE = "\u25B3";
-    /** Unicode character */ public static final String WHITE_DOWN_POINTING_TRIANGLE = "\u25BD";
+    /** Unicode character */ public static final String BLACK_DIAMOND = "\u25C6";
     /** Unicode character */ public static final String BLACK_LEFT_POINTING_TRIANGLE = "\u25C0";
+    /** Unicode character */ public static final String LIGHT_DOWN_AND_HORIZONTAL = "\u252C";
+    /** Unicode character */ public static final String LIGHT_DOWN_AND_LEFT = "\u2510";
+    /** Unicode character */ public static final String LIGHT_HORIZONTAL = "\u2500";
+    /** Unicode character */ public static final String LIGHT_RIGHT = "\u2576";
+    /** Unicode character */ public static final String LIGHT_UP_AND_HORIZONTAL = "\u2534";
+    /** Unicode character */ public static final String LIGHT_UP_AND_LEFT = "\u2518";
+    /** Unicode character */ public static final String LIGHT_VERTICAL = "\u2502";
+    /** Unicode character */ public static final String LIGHT_VERTICAL_AND_LEFT = "\u2524";
+    /** Unicode character */ public static final String WHITE_DIAMOND = "\u25C7";
+    /** Unicode character */ public static final String WHITE_DOWN_POINTING_TRIANGLE = "\u25BD";
+    /** Unicode character */ public static final String WHITE_UP_POINTING_TRIANGLE = "\u25B3";
 
     //-------------------------------------------------------------------------
     // Instance Variables
