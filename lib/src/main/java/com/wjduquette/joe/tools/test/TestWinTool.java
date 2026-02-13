5bf7d089afc9f835bab52fbe555dbca7df3a54b6
Will Duquette <will@wjduquette.com>
2026-02-13 09:29:45 -0800

    Added `--test` option to both `joe test` and `joe testwin`, allowing test filtering.

--- lib/src/main/java/com/wjduquette/joe/tools/test/TestWinTool.java
+++ lib/src/main/java/com/wjduquette/joe/tools/test/TestWinTool.java
@@ -35,14 +35,16 @@ public class TestWinTool implements Tool {
         
         Options:
         
+        --test name, -t name
+            Executes tests whose name contains the given value
+        --verbose, -v
+            Enable verbose output.
         --libpath path, --l path
            Sets the library path to the given path.
         --clark, -c
             Use the "Clark" byte-engine (default)
         --walker, -w
             Use the "Walker" AST-walker engine.
-        --verbose, -v
-            Enable verbose output.
         
         Test Scripts
         
@@ -68,6 +70,7 @@ public class TestWinTool implements Tool {
     // Instance Variables
 
     private String engineType = Joe.CLARK;
+    private String testName = null;
     private String libPath = null;
     private PackageFinder finder = null;
     private boolean verbose = false;
@@ -108,6 +111,7 @@ public class TestWinTool implements Tool {
             var arg = argq.poll();
 
             switch (arg) {
+                case "--test", "-t" -> testName = toOptArg(arg, argq);
                 case "--libpath", "-l" -> libPath = toOptArg(arg, argq);
                 case "--clark", "-c" -> engineType = Joe.CLARK;
                 case "--walker", "-w" -> engineType = Joe.WALKER;
@@ -190,6 +194,7 @@ public class TestWinTool implements Tool {
         // NEXT, execute its tests.
         var tests = joe.getVariableNames().stream()
             .filter(name -> name.startsWith("test"))
+            .filter(name -> testName == null || name.contains(testName))
             .toList();
 
         if (tests.isEmpty()) {
